820ae7c892e40faf9829306524c3c1c4b5607dc3
Bhumika Vaishay <124731977+bhumka@users.noreply.github.com>
2024-11-16 19:54:35 +0530

    Initial commit

---
+++ src/sections/SpecialOffer.jsx
@@ -0,0 +1,32 @@
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../components/Button"
+
+const SpecialOffer = () => {
+  return (
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse  gap-10 max-container">
+        <div className="flex-1">
+            <img src={offer} with={773} height={686} className="object-conatin w-full"/>
+        </div>
+
+        <div className="flex flex-1 flex-col">
+    
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red"> Special</span> Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Embarce the opportunity to get the best shoes at the best price. Our special offer is designed to give you the best value for your money. 
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of expectional</p>
+        <div className="mt-11 flex flex-wrap gap-4">
+            <Button label="View details"/>
+            <Button label="Learn more" backgroundColor="bg-white" borderColor="border-slate-gray" textColor="text-slate-gray"/>
+        </div>
+    
+      </div>
+
+    </section>
+  )
+}
+
+export default SpecialOffer
